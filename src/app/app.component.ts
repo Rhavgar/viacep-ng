31b9a36b44b4b2aa2806838c577916e4f3b23db4
Sidney Mathias <53013070+Rhavgar@users.noreply.github.com>
2021-11-11 14:53:43 -0300

    Viacep API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
-@Component({
+import {Form} from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+@Component
+({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'viacep-ng';
+export class AppComponent
+{
+  onSubmit(form: Form)
+  {
+    // console.log(form);
+  }
+
+  data: any = [];
+
+  viacep(cep: any)
+  {
+    if(cep != "")
+   {
+    this.http.get(`https://viacep.com.br/ws/${cep.value}/json/`).subscribe(search => {this.data = search});
+   }
+  }
+
+  constructor(private http: HttpClient) { }
 }
